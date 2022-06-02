c02a51c49e4b49fd4b91704f018477a39b7961ce
Antony Maziero Warken <againplayi7@gmail.com>
2022-06-02 15:54:09 -0300

    beleza

--- src/pages/Controlador/ConfirmarReset/index.tsx
+++ src/pages/Controlador/ConfirmarReset/index.tsx
@@ -1,30 +1,44 @@
 import classNames from "classnames";
-import { useEffect, useState } from "react";
-import styles from "./ConfirmarTransacao.module.scss";
+import styles from "./ConfirmarReset.module.scss";
 
 interface Props {
-  setTransacao: React.Dispatch<React.SetStateAction<boolean>>,
-  fechar: boolean,
-  setFechar: React.Dispatch<React.SetStateAction<boolean>>
+  setDeletar: React.Dispatch<React.SetStateAction<boolean>>;
+  fechar: boolean;
+  setFechar: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-
-export default function ConfirmarTransacao({ setTransacao, fechar, setFechar }: Props) {
+export default function ConfirmarTransacao({
+  setDeletar,
+  fechar,
+  setFechar,
+}: Props) {
   return (
-    <div className={classNames({
-      [styles.popup__div]: true,
-      [styles['popup__div--fechar']]: fechar === true ? true : false
-    })}>
+    <div
+      className={classNames({
+        [styles.popup__div]: true,
+        [styles["popup__div--fechar"]]: fechar === true ? true : false,
+      })}
+    >
       <div className={styles.popup__content}>
         <div className={styles.content__paragraph}>
-          Tem certeza que deseja prosseguir com a transação?
+          Tem certeza que deseja resetar os dados da aplicação?
         </div>
         <div className={styles.popup__opcoes}>
-          <button onClick={() => setFechar(true)} className={styles.opcoes__nao}>Não</button>
-          <button onClick={() => {
-            setTransacao(true)
-            setFechar(true)
-          }} className={styles.opcoes__sim}>Sim</button>
+          <button
+            onClick={() => setFechar(true)}
+            className={styles.opcoes__nao}
+          >
+            Não
+          </button>
+          <button
+            onClick={() => {
+              setDeletar(true);
+              setFechar(true);
+            }}
+            className={styles.opcoes__sim}
+          >
+            Sim
+          </button>
         </div>
       </div>
     </div>
--- src/pages/Controlador/Resetar/index.tsx
+++ src/pages/Controlador/Resetar/index.tsx
@@ -1,4 +1,6 @@
-import styles from './Resetar.module.scss'
+import { useEffect, useState } from "react";
+import ConfirmarTransacao from "../ConfirmarReset";
+import styles from "./Resetar.module.scss";
 
 interface Props {
   setEntradas: React.Dispatch<
@@ -15,29 +17,55 @@ interface Props {
       }[]
     >
   >;
-  setEstadoEntradas: React.Dispatch<React.SetStateAction<number>>
-  setEstadoSaidas: React.Dispatch<React.SetStateAction<number>>
+  setEstadoEntradas: React.Dispatch<React.SetStateAction<number>>;
+  setEstadoSaidas: React.Dispatch<React.SetStateAction<number>>;
 }
 
-const classNames = require('classnames')
+const classNames = require("classnames");
 
-export default function Resetar({ setEntradas, setSaidas, setEstadoEntradas, setEstadoSaidas }: Props) {
+export default function Resetar({
+  setEntradas,
+  setSaidas,
+  setEstadoEntradas,
+  setEstadoSaidas,
+}: Props) {
+  const [deletar, setDeletar] = useState(false);
+  const [fechar, setFechar] = useState(true);
+  useEffect(() => {
+    if (deletar === true) {
+      setEstadoEntradas(0);
+      setEstadoSaidas(0);
+      setEntradas([{ quantia: "" }]);
+      setSaidas([{ quantia: "" }]);
+      window.localStorage.removeItem("entradas");
+      window.localStorage.removeItem("saidas");
+    }
+  }, [setEstadoEntradas, setEstadoSaidas, setEntradas, setSaidas, deletar]);
   return (
     <div className={styles.resetar}>
-    <button className={classNames({
-      [styles.resetar__button]: true
-    })}
-      onClick={() => {
-        setEstadoEntradas(0)
-        setEstadoSaidas(0)
-        setEntradas([{"quantia": ''}])
-        setSaidas([{"quantia": ''}])
-        window.localStorage.removeItem("entradas");
-        window.localStorage.removeItem("saidas");
-      }}
-    >
-      Resetar
-    </button>
+      <ConfirmarTransacao
+        setDeletar={setDeletar}
+        fechar={fechar}
+        setFechar={setFechar}
+      ></ConfirmarTransacao>
+      <button
+        className={classNames({
+          [styles.resetar__button]: true,
+        })}
+        onClick={() => {
+          setFechar(false);
+          if (deletar === true) {
+            setEstadoEntradas(0);
+            setEstadoSaidas(0);
+            setEntradas([{ quantia: "" }]);
+            setSaidas([{ quantia: "" }]);
+            window.localStorage.removeItem("entradas");
+            window.localStorage.removeItem("saidas");
+          }
+        }}
+      >
+        Resetar
+      </button>
     </div>
   );
 }
